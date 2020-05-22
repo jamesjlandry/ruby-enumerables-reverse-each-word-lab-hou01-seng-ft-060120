e3943e98e06a5fae12ae4d721b659721bb02d95d
James Landry <james.j.landry@outlook.com>
2020-05-22 17:31:48 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+def reverse_each_word(string)
+  n_string = string.split
+  new_array = []
+  n_string.each do |jove| jove.reverse!
+  n_string.join(" ")
+  n_string << new_array
+  end
+  new_array
+end
